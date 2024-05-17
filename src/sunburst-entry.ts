a10bc6d92c20d54553bd455d3ba63b2248c9d103
automatictyping <xingyue.wang@hcu-hamburg.de>
2024-05-17 15:27:18 +0200

    Add sunburst html

---
+++ src/sunburst-entry.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SunburstControl from "./components/controls/SunburstControl.vue";
+
+createApp(SunburstControl).mount("#sunburst");
