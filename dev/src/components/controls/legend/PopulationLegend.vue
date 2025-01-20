dffccaab162cf16f1cffee3d2b52c5961dcaf450
automatictyping <xingyue.wang@hcu-hamburg.de>
2025-01-20 22:09:37 +0100

    update translations and color setting of legend chart

--- dev/src/components/controls/legend/PopulationLegend.vue
+++ dev/src/components/controls/legend/PopulationLegend.vue
@@ -1,23 +1,114 @@
 <script lang="ts" setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 import { useI18n } from "vue-i18n";
-const { locale } = useI18n();
 import * as echarts from "echarts";
-import usePopulationLegendStore from "@/stores/populationLegendStore";
+import {
+  populationInaccessibleColor,
+  populationAccessibleColor,
+} from "@/assets/ts/constants";
 
-const populationLegendStore = usePopulationLegendStore();
 // Population Legend Element
 const chartContainer = ref<HTMLDivElement | null>(null);
 let chart: echarts.ECharts;
-
+const { locale, t } = useI18n();
+// chart Options
+const gridData = [
+  [0, 0, populationInaccessibleColor[0]],
+  [0, 1, populationInaccessibleColor[1]],
+  [0, 2, populationInaccessibleColor[2]],
+  [0, 3, populationInaccessibleColor[3]],
+  [0, 4, populationInaccessibleColor[4]],
+  [1, 0, populationAccessibleColor[0]],
+  [1, 1, populationAccessibleColor[1]],
+  [1, 2, populationAccessibleColor[2]],
+  [1, 3, populationAccessibleColor[3]],
+  [1, 4, populationAccessibleColor[4]],
+];
+const populationLegendOption = computed(() => {
+  return {
+    grid: {
+      left: "10%",
+      right: 0,
+      top: "2%",
+      bottom: "7%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "category",
+      name: t("legend.population.accessible"),
+      nameLocation: "middle",
+      nameGap: 20,
+      data: ["No", "Yes"],
+      splitLine: { show: false },
+      nameTextStyle: {
+        color: "#2c3e50",
+        fontSize: 12,
+        fontFamily: "Arial",
+        fontStyle: "italic",
+      },
+      axisLabel: {
+        color: "#34495e",
+        fontSize: 12,
+        fontFamily: "Arial",
+        fontStyle: "normal",
+      },
+    },
+    yAxis: {
+      type: "category",
+      name: t("legend.population.population") + " / km\u00B2",
+      nameLocation: "middle",
+      nameGap: 25,
+      data: ["5", "15", "25", "35", ">35"],
+      splitLine: { show: false },
+      boundaryGap: true,
+      nameTextStyle: {
+        color: "#2c3e50",
+        fontSize: 12,
+        fontFamily: "Arial",
+        fontStyle: "italic",
+      },
+      axisLabel: {
+        color: "#34495e",
+        fontSize: 13,
+        fontFamily: "Arial",
+        fontStyle: "normal",
+      },
+    },
+    series: [
+      {
+        name: "Grid",
+        type: "custom",
+        renderItem: function (params: any, api: any) {
+          const xValue = api.value(0);
+          const yValue = api.value(1);
+          const start = api.coord([xValue, yValue]);
+          const size = api.size([1, 1]);
+          return {
+            type: "rect",
+            shape: {
+              x: start[0] - size[0] / 2,
+              y: start[1] - size[1] / 2,
+              width: size[0],
+              height: size[1],
+            },
+            style: {
+              fill: api.value(2),
+            },
+          };
+        },
+        data: gridData,
+      },
+    ],
+  };
+});
 // Methods
 const initChart = () => {
   chart = echarts.init(chartContainer.value!);
-  chart.setOption(populationLegendStore.populationLegendOption);
+  chart.setOption(populationLegendOption.value);
 };
 // Reload the chart when the language changes
 watch(locale, () => {
-  chart.setOption(populationLegendStore.populationLegendOption);
+  chart.setOption(populationLegendOption.value);
 });
 // Init Chart on mounted
 onMounted(initChart);
--- dev/src/components/controls/legend/WaterPointLegend.vue
+++ dev/src/components/controls/legend/WaterPointLegend.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 import { useI18n } from "vue-i18n";
 const { t, locale } = useI18n();
 import * as echarts from "echarts";
@@ -7,42 +7,44 @@ import * as echarts from "echarts";
 const chartContainer = ref<HTMLDivElement | null>(null);
 let chart: echarts.ECharts;
 
-const option = {
-  title: {
-    text: t("layerNames.kryvyirih.waterNetworkPoint").split(" ").join("\n"),
-    left: 40,
-    top: 5,
-    textStyle: {
-      color: "#3d3d3d",
-      fontSize: 10,
+const option = computed(() => {
+  return {
+    title: {
+      text: t("layerNames.kryvyirih.waterNetworkPoint").split(" ").join("\n"),
+      left: 40,
+      top: 0,
+      textStyle: {
+        color: "#3d3d3d",
+        fontSize: 10,
+      },
     },
-  },
-  visualMap: {
-    type: "continuous",
-    min: 0,
-    max: 0.45,
-    precision: 2,
-    inRange: {
-      color: ["#313695", "#7DDA58", "#FFDE59", "#D73027"], // Gradient colors
+    visualMap: {
+      type: "continuous",
+      min: 0,
+      max: 0.45,
+      precision: 2,
+      inRange: {
+        color: ["#313695", "#5DE2E7", "#7DDA58", "#FFDE59", "#D73027"], // Gradient colors
+      },
+      text: ["0.45", "0.0"], // Labels on the bar
+      textStyle: {
+        color: "#000",
+      },
+      orient: "vertical", // Vertical bar
+      left: 5, // Position
+      top: "middle",
     },
-    text: ["0.45", "0.0"], // Labels on the bar
-    textStyle: {
-      color: "#000",
-    },
-    orient: "vertical", // Vertical bar
-    left: 5, // Position
-    top: "middle",
-  },
-  series: [], // No actual data; just displaying the color bar
-};
+    series: [], // No actual data; just displaying the color bar
+  };
+});
 
 const initChart = () => {
   chart = echarts.init(chartContainer.value!);
-  chart.setOption(option);
+  chart.setOption(option.value);
 };
 // Reload the chart when the language changes
 watch(locale, () => {
-  chart.setOption(option);
+  chart.setOption(option.value);
 });
 onMounted(() => {
   initChart();
